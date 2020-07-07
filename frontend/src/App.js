2a99368192f384f183c094fed09f700a6d51edc2
Wanderson Castro <wandersonce1@hotmail.com>
2020-07-07 12:03:56 -0700

    sidebar menu functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+
   return (
     <div className="grid-container">
       <header className="header">
         <div className="brand">
-          <button onclick="openMenu()">
+          <button onClick={openMenu}>
             &#9776;
                 </button>
           <a href="index.html">WanderCommerce</a>
@@ -19,7 +28,7 @@ function App() {
       </header>
       <aside className="sidebar">
         <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onclick="closeMenu()">x</button>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
         <ul>
           <li>
             <a href="index.html">Pants</a>
