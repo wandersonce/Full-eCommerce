cafd1e913284cc8e4b194f9d8e1fff261bb6e0ad
Wanderson Castro <wandersonce1@hotmail.com>
2020-07-17 11:13:37 -0700

    Auth for admin and token added

--- backend/util.js
+++ backend/util.js
@@ -17,4 +17,30 @@ const getToken = (user) => {
     );
 };
 
-export { getToken }
+const isAuth = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        const onlyToken = tone.slice(7, token.length) //* Getting token from frontend login
+        jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => { //* Authentication of the token
+
+            if (err) {
+                return res.status(401).send({ msg: 'Invalid Token' })
+            }
+
+            req.user = token;
+            next();
+            return
+        })
+    }
+
+    return res.status(401).send({ msg: "Token is not supplied." })
+}
+
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        return next();
+    }
+    return res.status(401).send({ msg: "Admin Token is not valid." })
+}
+
+export { getToken, isAuth, isAdmin }
